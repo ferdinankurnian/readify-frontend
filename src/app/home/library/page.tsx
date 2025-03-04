6feec6b84b42d698c06a62fc26c856ea62b41c38
Ferdinan Kurnia <ferdinankurnian@gmail.com>
2025-03-04 22:28:30 +0700

    Added login page

---
+++ src/app/home/library/page.tsx
@@ -0,0 +1,113 @@
+import type { Metadata } from 'next';
+import Link from 'next/link';
+
+export const metadata: Metadata = {
+    title: 'Library - Readify',
+    description: 'Library Page',
+};
+
+export default function MyBooks() {
+    return (
+        <>
+            <div className="flex h-screen">
+                <div className="w-24 bg-[#324549] flex flex-col items-center justify-between py-4 space-y-4">
+                    <button className="p-3 rounded-lg ">
+                        <img src="../favicon.ico" width={40} alt="Icon" />
+                    </button>
+                    <div className="flex flex-col items-center justify-center py-4 space-y-4">
+                        <button className="p-3 rounded-lg bg-[#1D282C] active:scale-90 transition-transform">
+                            <span className="material-symbols-outlined filled text-[32px]">
+                                home
+                            </span>
+                        </button>
+                        <Link href="/mybooks">
+                            <button className="p-3 rounded-lg hover:bg-[#1D282C] active:scale-90 transition-transform">
+                                <span className="material-symbols-outlined text-[32px]">
+                                    auto_stories
+                                </span>
+                            </button>
+                        </Link>
+                        <Link href="/history">
+                            <button className="p-3 rounded-lg hover:bg-[#1D282C] active:scale-90 transition-transform">
+                                <span className="material-symbols-outlined text-[32px]">
+                                    search_activity
+                                </span>
+                            </button>
+                        </Link>
+                        <Link href="/wishlist">
+                            <button className="p-3 rounded-lg hover:bg-[#1D282C] active:scale-90 transition-transform">
+                                <span className="material-symbols-outlined text-[32px]">
+                                    favorite
+                                </span>
+                            </button>
+                        </Link>
+                        <button className="p-3 rounded-lg hover:bg-[#1D282C] active:scale-90 transition-transform">
+                            <span className="material-symbols-outlined text-[32px]">
+                                notifications
+                            </span>
+                        </button>
+                    </div>
+                    <button className="p-3 rounded-lg hover:bg-[#1D282C] active:scale-90 transition-transform">
+                        <span className="material-symbols-outlined text-[32px]">
+                            settings
+                        </span>
+                    </button>
+                </div>
+                <div className="flex-1 p-10 py-5">
+                    <div className="flex justify-between items-center mb-6">
+                        <div className="flex space-x-8">
+                            <a className="text-white text-lg" href="#">
+                                Library
+                            </a>
+                            <Link className="text-gray-400 text-lg hover:text-gray-300" href="books">
+                                Books
+                            </Link>
+                            <Link className="text-gray-400 text-lg hover:text-gray-300" href="categories">
+                                Categories
+                            </Link>
+                        </div>
+                        <div>
+                            <input
+                                className="bg-gray-700 text-white rounded-full px-4 py-2"
+                                placeholder="Search here"
+                                type="text"
+                            />
+                        </div>
+                    </div>
+                    <div className="bg-gray-700 rounded-lg h-48 mb-6"></div>
+                    <div>
+                        <h2 className="text-3xl font-bold mb-4">Continue Reading</h2>
+                        <div className="flex space-x-4">
+                            <button className="flex flex-col w-48 bg-[#324549] rounded-lg overflow-hidden shadow-lg max-w-xs">
+                                <img
+                                    alt="Book cover of 'From Zero to Millionaire' by Nicolas Bérubé"
+                                    className="w-[200px] h-[200px]"
+                                    src="https://storage.googleapis.com/a1aa/image/T-sDq3H1iX0rU9SZhBQyMC9PCu3Zr52Fagk5rLcjtNc.jpg"
+                                />
+                                <div className="flex flex-col gap-1 p-3 pt-2 text-left">
+                                    <h2 className="text-white text-md font-semibold">
+                                        From Zero to Millionaire
+                                    </h2>
+                                    <p className="text-sm text-gray-400">Ferry Unardi</p>
+                                </div>
+                            </button>
+                            <button className="flex flex-col w-48 bg-[#324549] rounded-lg overflow-hidden shadow-lg max-w-xs">
+                                <img
+                                    alt="Book cover of 'From Zero to Millionaire' by Nicolas Bérubé"
+                                    className="w-[200px] h-[200px]"
+                                    src="https://storage.googleapis.com/a1aa/image/vUCuq67Q3x0voyb4_jgAVwpEjKVfQ8dyTzW3sbwd9aA.jpg"
+                                />
+                                <div className="flex flex-col gap-1 p-3 pt-2 text-left">
+                                    <h2 className="text-white text-md font-semibold">
+                                        The Zurich Axioms
+                                    </h2>
+                                    <p className="text-sm text-gray-400">Max Gunther</p>
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,103 +6,54 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-      <div className="flex h-screen">
-        <div className="w-24 bg-[#324549] flex flex-col items-center justify-between py-4 space-y-4">
-          <button className="p-3 rounded-lg ">
-            <img src="favicon.ico" width={40} alt="Icon" />
-          </button>
-          <div className="flex flex-col items-center justify-center py-4 space-y-4">
-            <button className="p-3 rounded-lg bg-[#1D282C] active:scale-90 transition-transform">
-              <span className="material-symbols-outlined filled text-[32px]">
-                home
-              </span>
-            </button>
-            <Link href="/mybooks">
-              <button className="p-3 rounded-lg hover:bg-[#1D282C] active:scale-90 transition-transform">
-                <span className="material-symbols-outlined text-[32px]">
-                  auto_stories
-                </span>
-              </button>
-            </Link>
-            <Link href="/history">
-            <button className="p-3 rounded-lg hover:bg-[#1D282C] active:scale-90 transition-transform">
-              <span className="material-symbols-outlined text-[32px]">
-                search_activity
-              </span>
-            </button>
-            </Link>
-            <Link href="/wishlist">
-            <button className="p-3 rounded-lg hover:bg-[#1D282C] active:scale-90 transition-transform">
-              <span className="material-symbols-outlined text-[32px]">
-                favorite
-              </span>
-            </button>
-            </Link>
-            <button className="p-3 rounded-lg hover:bg-[#1D282C] active:scale-90 transition-transform">
-              <span className="material-symbols-outlined text-[32px]">
-                notifications
-              </span>
-            </button>
+      <div className="flex w-full min-h-screen">
+        <div className="w-full md:w-1/2 p-10">
+          <div className="flex items-end justify-between mb-12">
+            <div className="flex items-center space-x-2">
+              <img src="/readifybrand.svg" alt="My Icon" height="100" />
+            </div>
+            <a className="text-gray-400 p-2 px-3 rounded-md hover:text-white hover:bg-gray-600" href="#">
+              Sign up
+            </a>
           </div>
-          <button className="p-3 rounded-lg hover:bg-[#1D282C] active:scale-90 transition-transform">
-            <span className="material-symbols-outlined text-[32px]">
-              settings
-            </span>
-          </button>
+          <div className="flex flex-col">
+            <h2 className="text-3xl font-bold mb-2">
+              Sign In
+            </h2>
+            <p className="text-gray-400 mb-8">
+              Sign In to an account
+            </p>
+            <form>
+              <div className="mb-4">
+                <label className="block text-gray-400 mb-2">
+                  Email
+                </label>
+                <div className="flex items-center border border-gray-600 rounded-lg overflow-hidden px-3">
+                  <span className="material-symbols-outlined opacity-[0.75]">
+                    mail
+                  </span>
+                  <input className="w-full px-3 py-2 bg-gray-800 text-white focus:outline-none" id="email" placeholder="name@example.com" type="email" />
+                </div>
+              </div>
+              <div className="mb-6">
+                <label className="block text-gray-400 mb-2">
+                  Password
+                </label>
+                <div className="flex items-center border border-gray-600 rounded-lg overflow-hidden px-3">
+                  <span className="material-symbols-outlined opacity-[0.75]">
+                    lock
+                  </span>
+                  <input className="w-full px-3 py-2 bg-gray-800 text-white focus:outline-none" id="password" placeholder="********" type="password" />
+                </div>
+              </div>
+              <button className="w-full bg-gray-700 hover:bg-gray-600 text-white py-2 rounded-lg" type="submit">
+                Sign in
+              </button>
+            </form>
+          </div>
         </div>
-        <div className="flex-1 p-10 py-5">
-          <div className="flex justify-between items-center mb-6">
-            <div className="flex space-x-8">
-              <a className="text-white text-lg" href="#">
-                Library
-              </a>
-              <a className="text-gray-400 text-lg hover:text-gray-300" href="#">
-                Books
-              </a>
-              <a className="text-gray-400 text-lg hover:text-gray-300" href="#">
-                Categories
-              </a>
-            </div>
-            <div>
-              <input
-                className="bg-gray-700 text-white rounded-full px-4 py-2"
-                placeholder="Search here"
-                type="text"
-              />
-            </div>
-          </div>
-          <div className="bg-gray-700 rounded-lg h-48 mb-6"></div>
-          <div>
-            <h2 className="text-3xl font-bold mb-4">Continue Reading</h2>
-            <div className="flex space-x-4">
-              <button className="flex flex-col w-48 bg-[#324549] rounded-lg overflow-hidden shadow-lg max-w-xs">
-                <img
-                  alt="Book cover of 'From Zero to Millionaire' by Nicolas Bérubé"
-                  className="w-[200px] h-[200px]"
-                  src="https://storage.googleapis.com/a1aa/image/T-sDq3H1iX0rU9SZhBQyMC9PCu3Zr52Fagk5rLcjtNc.jpg"
-                />
-                <div className="flex flex-col gap-1 p-3 pt-2 text-left">
-                  <h2 className="text-white text-md font-semibold">
-                    From Zero to Millionaire
-                  </h2>
-                  <p className="text-sm text-gray-400">Ferry Unardi</p>
-                </div>
-              </button>
-              <button className="flex flex-col w-48 bg-[#324549] rounded-lg overflow-hidden shadow-lg max-w-xs">
-                <img
-                  alt="Book cover of 'From Zero to Millionaire' by Nicolas Bérubé"
-                  className="w-[200px] h-[200px]"
-                  src="https://storage.googleapis.com/a1aa/image/vUCuq67Q3x0voyb4_jgAVwpEjKVfQ8dyTzW3sbwd9aA.jpg"
-                />
-                <div className="flex flex-col gap-1 p-3 pt-2 text-left">
-                  <h2 className="text-white text-md font-semibold">
-                  The Zurich Axioms
-                  </h2>
-                  <p className="text-sm text-gray-400">Max Gunther</p>
-                </div>
-              </button>
-            </div>
-          </div>
+        <div className="hidden md:block md:w-1/2">
+          <img alt="A stack of books with an open book on top in a library setting" className="w-full h-full object-cover rounded-l-[50px]" height="800" src="https://storage.googleapis.com/a1aa/image/a39XT4Ih73lIZV3lqGYOU7y0o20KgEqZNNe3_vos2SA.jpg" width="600" />
         </div>
       </div>
     </>
